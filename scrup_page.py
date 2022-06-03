3cc89944f65fc10147460d9bb7534ff9b935b91d
LeX1978 <alexey.orlov78@gmail.com>
2022-06-03 15:11:51 +0300

    1. Немного оптимизировал процедуры 2. Эмуляция чела

--- scrup_page.py
+++ scrup_page.py
@@ -1,8 +1,9 @@
 import requests
 from requests.exceptions import HTTPError
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import re
-from fake_useragent import UserAgent
+import time
+
 
 page_list = set()
 url = "https://www.cian.ru/cat.php?currency=2&deal_type=sale&demolished_in_moscow_programm=0&engine_version=2&is_first_floor=0&maxprice=24000000&mintarea=80&object_type%5B0%5D=1&offer_type=flat&only_flat=1&p=1&region=1&room3=1"
@@ -11,26 +12,41 @@ stop_flag = False
 # Установка соединения с сайтом
 def setConnection(url):
     session = requests.Session()
-    ua = UserAgent().chrome
-    hdr = {'User-Agent' : ua,
-    	       'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
+    hdr = {'authority': 'top-fwz1.mail.ru',
+            'method': 'POST',
+            'path': '/tracker?js=13;id=2775447;u=https%3A//www.cian.ru/cat.php%3Fcurrency%3D2%26deal_type%3Dsale%26demolished_in_moscow_programm%3D0%26engine_version%3D2%26is_first_floor%3D0%26maxprice%3D24000000%26mintarea%3D80%26object_type%255B0%255D%3D1%26offer_type%3Dflat%26only_flat%3D1%26p%3D1%26region%3D1%26room3%3D1;st=1654099866688;title=%D0%9A%D1%83%D0%BF%D0%B8%D1%82%D1%8C%203-%D0%BA%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%BD%D1%83%D1%8E%20%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D1%83%20%D0%B2%20%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B5%20-%20927%20%D0%BE%D0%B1%D1%8A%D1%8F%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B9;s=1920*1080;vp=939*915;touch=0;hds=1;frame=0;flash=;sid=d1b945b7f28e6e80;ver=60.3.0;tz=-180%2FEurope%2FMoscow;ni=10//4g/0/0/;detect=0;lvid=1648888798200%3A1654099883576%3A263%3A8f638f50d6b2831c3b3cc9cec5115878;opts=dl%2Cjst-gtag-ga;visible=true;_=0.6560150825955811;e=PVT/15',
+            'scheme': 'https',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'ru,en;q=0.9',
+            'content-length': '0',
+            'content-type': 'text/plain;charset=UTF-8',
+            'cookie': 'p=l70AAGHC8OYA; searchuid=5676853861646464360; b=i0oCAKBgnW0Ae0oOCAEAAAinR8iF4XRNVDMA; c=xpBhYgAAAD/1aAIRAAQAtAABAAIA; _ym_d=1653584997; _ym_uid=1653584997474935865; i=AQATj5diCAATAAiCK3YAAc4AAdkAARwBAR8BAUUBARcEAUYEAVsEAYsEATgFATkFARAGAbIHAVoIAYAIAYEIAYIIAYYIATwJAS0LAUILAXcLAVgMAZcMAfUMAfYMAfcMARgNARkNAXoOAYsOAYwOAY0OAY4OAZEOAZwOAZ4OAaEOAXseAfUgAfYgAfEiAbsBCAQBAQABkwIIYSBtAAGkAQEDAgEEAgEHAgEJAgEPAgESAgEXAgFuAwETBAFKBQFPBQFgBQFtBQFxBQF1BQGgBQGhBQGjBQGmBQGpBQGBBgHFCwHICwHJCwHMCwHOCwFzDQF4DQGVDQGiYwHcBAgEAQEAAeEECQEB4gQKBBoCugfWBggEAQEAAb0HCAQBghUB; act=d59bc23967424111ab1898c170343392; VID=1MQ7eq1lyG2A00000c1CH4oA:::0-0-7b03d5c-761c3cd:CAASEITeco0rxo3DfLrEcmpcqFoacN8C9Ulmrps6rpyhpFHGBfWFv8ogFVqOOtf9FmS1oTaVyaL2fF6ixBfrmetU9Qr7riHotLN1ofxGZAyIMhspt5i1QzaKNpt19xCFEsu_p-1kCvxNkxxOGlj-CgkltubD_CWolDz1pKEdSM9Ewv3f6gU',
+            'origin': 'https://www.cian.ru',
+            'referer': 'https://www.cian.ru/',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Yandex";v="22"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'no-cors',
+            'sec-fetch-site': 'cross-site',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.160 YaBrowser/22.5.1.966 Yowser/2.5 Safari/537.36'
+    }
+
     try:
         response = session.get(url, headers = hdr)
-        print(response)
-        for key, value in response.request.headers.items():
-            print(key + ": " + value)
         response.raise_for_status()
     except HTTPError as http_err:
         print(f'HTTP error occured: {http_err}')
     except Exception as err:
         print(f'Other error occured: {err}')
     else:
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = bs(response.text, 'html.parser')
         return soup
 
 #Процедура поиска ссылок на страницы
-def pageLinks(url,soup):
+def pageLinks(soup):
     link = ""
+    url = ""
     pages = soup.findAll("a", {'class': '_93444fe79c--list-itemLink--BU9w6'})
     for p_link in pages:
         page_num = p_link.text
@@ -41,7 +57,6 @@ def pageLinks(url,soup):
         if page_num != "..":
             if link not in page_list:
                 page_list.add(link)
-                print (page_num + " - " + link)
         else:
             url = link
     return url
@@ -81,20 +96,20 @@ def getFlatInfo(soup):
 ################################################################################
 ## Основная программа
 ################################################################################
-bf = setConnection(url)
-print(bf)
 
 #Собираем все ссылки на страницы квартир
-#while stop_flag is False:
-#    if url not in page_list:
-#        page_list.add(url)
-#        bf = setConnection(url)
-#        url = pageLinks(url,bf)
-#    else:
-#        stop_flag = True
+while stop_flag is False:
+   if url not in page_list:
+        page_list.add(url)
+        bf = setConnection(url)
+        url = pageLinks(bf)
+   else:
+        stop_flag = True
+   time.sleep(8)  # Сон в 3 секунды
 
 #Берем список линков и по каждой собираем инфу о квартире
-#for link in page_list:
-#    print(link)
-#    bf = setConnection(link)
-#    getFlatInfo(bf)
+for link in page_list:
+    print(link)
+    bf = setConnection(link)
+    getFlatInfo(bf)
+    time.sleep(8)
