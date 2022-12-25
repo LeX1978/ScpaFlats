93ee194308bad606fb054adec514869f858ec5c6
LeX1978 <alexey.orlov78@gmail.com>
2022-12-25 18:36:04 +0300

    ver new

--- сian_scrup_selen.py
+++ сian_scrup_selen.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 from bs4 import BeautifulSoup as bs
+import re
 
 page_list = set()
 page_url = 'https://www.cian.ru/cat.php?currency=2&deal_type=sale&demolished_in_moscow_programm=0&engine_version=2' \
@@ -13,7 +14,7 @@ def get_session(url):
     EXE_PATH = r'C:\Work\python\chromedriver.exe'
     driver = webdriver.Chrome(executable_path=EXE_PATH)
     driver.get(url)
-    time.sleep(8)
+    time.sleep(7)
     driver.get(url)
     response = driver.page_source
     soup = bs(response, 'html.parser')
@@ -39,6 +40,37 @@ def page_links(soup):
             p_url = link
     return p_url
 
+# Поцедура получения информации о квартирах на странице
+def get_flatinfo(soup):
+    # Находим все блоки с информацией о квартире
+    blocks = soup.findAll("div", {'data-name': 'LinkArea'})
+    for block in blocks:
+        # Определяем ссылку на конкретную квартиру
+        link_flat = block.find('a', class_='_93444fe79c--link--eoxce')
+        if link_flat is not None:
+            if 'href' in link_flat.attrs:
+                link = link_flat.attrs['href']
+                match = re.search(r'\d{9}', link)
+                flat_id = match[0] if match else 0
+                print(flat_id)
+        # Ищем название ссылки
+        link_spans = block.find("span", {'class': ''})
+        link_text = link_spans.get_text().encode("utf8")
+        link_text_decoded = link_text.decode('utf8')
+        print(link_text_decoded)
+
+        # Ищем метро
+        metro_block = block.findAll("a", {'class': '_93444fe79c--link--BwwJO'})
+        for metro in metro_block:
+            metro_name = metro.find("div", {'class': ''})
+            print(metro_name.text)
+        # Ищем цену
+        price_block = block.findAll("span", {'data-mark': 'MainPrice'})
+        for price in price_block:
+            price_num = price.find("span")
+            flat_price = price_num.get_text()
+            print(flat_price)
+
 
 def collect_data():
     global page_url
@@ -53,12 +85,14 @@ def collect_data():
             page_url = page_links(bf)
         else:
             stop_flag = True
-        time.sleep(8)  # Сон в 8 секунды
 
+    # Берем список линков и по каждой собираем инфу о квартире
+    for link in page_list:
+        bf = get_session(link)
+        get_flatinfo(bf)
 
 def main():
     collect_data()
-
 
 if __name__ == '__main__':
     main()
